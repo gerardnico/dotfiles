cb744c8282e2d3cbedb1ac1ea6ab76236ac79e39
Nicolas GERARD <gerardnico@gmail.com>
2024-10-01 17:59:42 +0200

    Home brew

---
+++ dot_bashrc.d/bin.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Add all repo/bin directory into the PATH
+
+# Find all bin directories in the second level of the code directory
+add_bin_dirs_from_code_repo() {
+    local BASE_DIR=$1
+
+    # Use find to locate bin directories
+    while IFS= read -r bin_dir; do
+        # If this is a directory
+        if [ -d "$bin_dir" ]; then
+            if [[ ":$PATH:" != *":$bin_dir:"* ]]; then
+                export PATH="$1:$PATH"
+            fi
+        fi
+    done < <(find "$BASE_DIR" -maxdepth 2 -type d -name "bin")
+
+}
+
+# Execute the function
+add_bin_dirs_from_code_repo "$HOME"/code
---
+++ dot_bashrc.d/homebrew.sh
@@ -0,0 +1,6 @@
+
+
+if [ -f /home/linuxbrew/.linuxbrew/bin/brew ]; then
+  # add Homebrew to your PATH
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
