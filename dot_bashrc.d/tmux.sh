353c3bbe3bc09993bb227001e806c88b74681d0f
Nicolas GERARD <gerardnico@gmail.com>
2024-10-10 17:44:46 +0200

    Added tmux at start

--- dot_bashrc.d/tmux.sh
+++ dot_bashrc.d/tmux.sh
@@ -1,16 +1,17 @@
 
 
+#############################
+# Tmux
+#############################
 if [ ! -x "$(command -v tmux)" ]; then
   echo "Tmux is not installed "
   echo "https://github.com/tmux/tmux/wiki/Installing"
-  echo "sudo apt install -y tmux"
-  echo "alias tmux=\"tmux attach\""
-  return
+  echo "On debian"
+  echo "  sudo apt install -y tmux"
 fi
 
-# If tmux is invoked without arguments, it will create a new session.
-# we prevent it with this alias
-alias tmux="tmux attach"
-
-
+# We can't start tmux in bashrc because
+# when tmux create a pane, it starts a login bash
+# (and we call bashrc from /etc/profile)
+# making it nested
 
