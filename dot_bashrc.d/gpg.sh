05673c114c0f17ebc602cf0078a2c8091b1e5b26
Nicolas GERARD <gerardnico@gmail.com>
2024-10-18 19:19:50 +0200

    Backup

--- dot_bashrc.d/gpg.sh
+++ dot_bashrc.d/gpg.sh
@@ -4,3 +4,10 @@
 # https://gnupg.org/documentation/manuals/gnupg/Invoking-GPG_002dAGENT.html
 GPG_TTY=$(tty)
 export GPG_TTY
+
+# If you enabled the Ssh Agent Support, you also need to tell ssh about it by adding this to your init script:
+# From: https://gnupg.org/documentation/manuals/gnupg/Agent-Examples.html
+# unset SSH_AGENT_PID
+# if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+#  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+# fi
