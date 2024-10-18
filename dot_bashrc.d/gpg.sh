c24f6b894cd04a2a39fc6b2916164fff9c1baab9
Nicolas GERARD <gerardnico@gmail.com>
2024-10-18 18:52:53 +0200

    Backup

---
+++ dot_bashrc.d/gpg.sh
@@ -0,0 +1,6 @@
+
+# Gpg
+# As seen here:
+# https://gnupg.org/documentation/manuals/gnupg/Invoking-GPG_002dAGENT.html
+GPG_TTY=$(tty)
+export GPG_TTY
--- dot_bashrc.d/ls.sh
+++ dot_bashrc.d/ls.sh
@@ -6,6 +6,6 @@
 alias ls='ls -hF --color=tty'                 # classify files in colour
 alias dir='ls --color=auto --format=vertical'
 # alias vdir='ls --color=auto --format=long'
-alias ll='ls -l'                              # long list
+alias ll='ls -al'                              # long list with . files
 alias la='ls -A'                              # all but . and ..
 alias l='ls -CF'                              #
