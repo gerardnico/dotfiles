e2d04a4289740cc207511a0165c6d11568db5fe6
Nicolas GERARD <gerardnico@gmail.com>
2024-10-21 12:18:26 +0200

    Package install

---
+++ dot_bashrc.d/chezmoi.sh
@@ -0,0 +1,4 @@
+
+# https://www.chezmoi.io/user-guide/frequently-asked-questions/usage/#how-do-i-enable-shell-completions
+# We didn't installed it with the package manager
+eval "$(chezmoi completion bash)"
