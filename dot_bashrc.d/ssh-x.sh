073c879f0aca4bb1b486c914385539f8c8ae29fb
Nicolas GERARD <gerardnico@gmail.com>
2024-10-31 19:23:58 +0100

    SSH on the road

--- dot_bashrc.d/ssh-x.sh
+++ dot_bashrc.d/ssh-x.sh
@@ -1,14 +1,15 @@
 ##############################
 # SSH Env
 ##############################
-# Depend on https://github.com/gerardnico/bash-lib/lib/ssh.sh
-# We start the SSH agent and load the keys
 
-source bashlib-ssh.sh
 
 # The location of the env file
-export SSH_ENV="$HOME"/.ssh/ssh-agent.env
+export SSH_X_ENV="$HOME"/.ssh/ssh-agent.env
 # The location of the agent socket
 export SSH_AUTH_SOCK="$HOME"/.ssh/agent.sock
+ssh-x-agent-init
 
-ssh::agent_init
+
+# An alias
+alias ssx="ssh-x"
+
