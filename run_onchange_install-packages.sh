9909e45f95e7af645b30d149efba3b69d871b9e3
Nicolas GERARD <gerardnico@gmail.com>
2024-10-31 16:44:18 +0100

    Ssh on the road

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -14,5 +14,115 @@ fi
 # If the output is empty, the script is not run
 # On Windows, we get `%1 is not a valid Win32 application.`
 
-# To get the syntax highlighting we moved the installation script
-bin/install-all
+# Install Package
+# https://www.chezmoi.io/user-guide/use-scripts-to-perform-actions/#install-packages-with-scripts
+# You can do it also declaratively by using a yaml file [Package](https://www.chezmoi.io/user-guide/advanced/install-packages-declaratively/)
+# Run with `chezmoi apply` or `chezmoi update`
+
+# The file run only if it has changed
+# You can delete the script cache to run it again without changing the content
+# See https://www.chezmoi.io/user-guide/use-scripts-to-perform-actions/#run-a-script-when-the-contents-of-another-file-changes
+#
+echo "Install my Packages"
+
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+install_git_extras(){
+  ## https://github.com/tj/git-extras/blob/main/Installation.md
+  ## Only brew is maintained by the author
+  if ! git standup; then
+    echo "Installing Git Extras"
+    brew install git-extras
+  else
+    echo "Git Extra founds"
+  fi
+}
+
+
+if ! command_exists git; then
+  echo "Installing Git"
+  sudo apt install git
+else
+  echo "Git Found"
+fi
+
+# cd on
+if ! command_exists zoxide; then
+  echo "Installing Zoxide"
+  brew install zoxide
+else
+  echo "Zoxide Found"
+fi
+
+# fuzzy finder
+if ! command_exists fzf; then
+  echo "Installing Fzf"
+  brew install fzf
+else
+  echo "Fzf Found"
+fi
+
+# rsync
+if ! command_exists rsync; then
+  echo "Installing Rsync"
+  sudo apt install -y rsync
+else
+  echo "Rsync Found"
+fi
+
+# tmux
+if ! command_exists tmux; then
+  echo "Installing Tmux"
+  # https://github.com/tmux/tmux/wiki/Installing
+  sudo apt install -y tmux
+  echo "Tmux: Don't forget to disable the ALT+Fn shortcut on MinTty Wsl/Cygwin"
+else
+  echo "Tmux Found"
+fi
+
+# Call Install function
+install_git_extras
+
+if ! command_exists lazygit; then
+  echo "Installing LazyGit"
+  brew install jesseduffield/lazygit/lazygit
+else
+  echo "LazyGit Found"
+fi
+
+if ! command_exists nc; then
+  sudo apt install -y netcat-openbsd
+else
+  echo "Netcat installed"
+fi
+
+# Needed with ssh
+if ! command_exists ssh-askpass; then
+  sudo apt install -y ssh-askpass
+else
+  echo "ssh-askpass installed"
+fi
+
+# https://mailutils.org
+if ! command_exists mail; then
+  sudo apt install -y mailutils
+  sudo apt install -y libmailutils-dev # mailutils command
+  sudo apt install -y mailutils-mda # putmail
+else
+  echo "Mailutils installed"
+fi
+
+if ! command_exists telnet; then
+  sudo apt install -y telnet
+else
+  echo "Telnet installed"
+fi
+
+if ! command_exists envsubst; then
+  sudo apt install -y gettext
+else
+  echo "GetText envsubst installed"
+fi
