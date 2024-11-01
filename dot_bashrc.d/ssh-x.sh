ffc9b679268867fc5cdee61e917c72927aedacd9
Nicolas GERARD <gerardnico@gmail.com>
2024-11-01 12:17:08 +0100

    Ssh on the road

--- dot_bashrc.d/ssh-x.sh
+++ dot_bashrc.d/ssh-x.sh
@@ -3,12 +3,7 @@
 ##############################
 
 
-# The location of the env file
-export SSH_X_ENV="$HOME"/.ssh/ssh-agent.env
-# The location of the agent socket
-export SSH_AUTH_SOCK="$HOME"/.ssh/agent.sock
 ssh-x-agent-init
-
 
 # An alias
 alias ssx="ssh-x"
