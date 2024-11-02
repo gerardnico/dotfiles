d62776f50830db26677cfeb96c90a3ef9b2e9cac
Nicolas GERARD <gerardnico@gmail.com>
2024-11-02 20:37:59 +0100

    Recursion probleme man !

--- dot_bashrc.d/ssh-x.sh
+++ dot_bashrc.d/ssh-x.sh
@@ -2,8 +2,9 @@
 # SSH Env
 ##############################
 
-
-ssh-x-agent-init
+# Disable for the moment due to the recursion
+# ssh-x-agent-init
+# eval $(cat /home/admin/.ssh/ssh-x-agent.env)
 
 # An alias
 alias ssx="ssh-x"
