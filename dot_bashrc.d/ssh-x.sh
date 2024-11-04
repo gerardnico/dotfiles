144ab7240a66bc9eb88e7b70fef733e2ce852467
Nicolas GERARD <gerardnico@gmail.com>
2024-11-04 10:50:39 +0100

    Ssh on the road

--- dot_bashrc.d/ssh-x.sh
+++ dot_bashrc.d/ssh-x.sh
@@ -2,9 +2,8 @@
 # SSH Env
 ##############################
 
-# Disable for the moment due to the recursion
-# ssh-x-agent-init
-# eval $(cat /home/admin/.ssh/ssh-x-agent.env)
+# Start the agent
+eval $(ssh-x-agent-init)
 
 # An alias
 alias ssx="ssh-x"
