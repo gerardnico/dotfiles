da83443aab6e359f94dc036b489065ed40004868
Nicolas GERARD <gerardnico@gmail.com>
2024-11-06 17:12:12 +0100

    Implemented pass as key store

--- dot_bashrc.d/ssh-x.sh
+++ dot_bashrc.d/ssh-x.sh
@@ -8,6 +8,9 @@ if ! eval "$(ssh-x-agent-init)"; then
   echo "SSH Agent Started Not successfully" >/dev/tty
 fi
 
+# Private Key in pass
+export SSH_X_KEY_STORE="pass"
+
 # An alias
 alias ssx="ssh-x"
 
