ddece11d328c4e1260d33098fed40a39916db704
Nicolas GERARD <gerardnico@gmail.com>
2024-11-07 19:34:29 +0100

    Git Hosting on the run

---
+++ dot_bashrc.d/dock-x.sh
@@ -0,0 +1,8 @@
+
+# Default
+export DOCK_X_NAMESPACE=gerardnico # the namespace of the image
+export DOCK_X_REGISTRY=ghcr.io # the registry of the image
+
+# Alias
+alias dx="dock-x"
+
--- dot_bashrc.d/dockenv.sh
+++
@@ -1,7 +0,0 @@
-
-# Default
-export DOCKER_NAMESPACE=gerardnico # the namespace of the image
-export DOCKER_REGISTRY=ghcr.io # the registry of the image
-
-# Alias
-alias dk="dockenv"
