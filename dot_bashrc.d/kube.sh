4724bf8a7f332a33c61f814f498afa6eb31cc579
Nicolas GERARD <gerardnico@gmail.com>
2024-11-11 10:21:51 +0100

    Kube on the road

--- dot_bashrc.d/kube.sh
+++ dot_bashrc.d/kube.sh
@@ -25,7 +25,7 @@ fi
 # Kube Utilities
 # https://github.com/gerardnico/kube-x
 ##############################
-export KUBE_X_APP_HOME="$HOME/code/argocd"
+export KUBE_X_APP_HOME="$HOME/code/argocd:$HOME/code/infra/kustomize"
 export KUBE_X_CLUSTER="beau"
 export KUBE_X_USER="default"
 export KUBE_X_KUBECTL="kubectx"
