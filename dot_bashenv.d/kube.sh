d83920a777681cab451966e611df1f85527ed498
Nicolas GERARD <gerardnico@gmail.com>
2024-11-18 21:28:47 +0100

    Alert on the road

--- dot_bashenv.d/kube.sh
+++ dot_bashenv.d/kube.sh
@@ -8,3 +8,5 @@ export KUBE_X_CLUSTER="beau"
 export KUBE_X_USER="default"
 export KUBE_X_KUBECTL="kubectx"
 export KUBE_X_BUSYBOX_IMAGE="ghcr.io/gerardnico/busybox@sha256:28e4627ee3c9f96eb36cb80de1acd7a8aa1e54b0940012ddb748ace5789954d6"
+
+export KUBI_ALERT_MANAGER_API_URL=https://alertmanager.eraldy.com/api/v2
