60e69e10130f22ef7be8fb05533f3c94b17ab1f4
Nicolas GERARD <gerardnico@gmail.com>
2024-11-22 10:57:04 +0100

    Moving all kustomize app in the same repo

--- dot_bashenv.d/kube.sh
+++ dot_bashenv.d/kube.sh
@@ -3,7 +3,7 @@
 # Kube Utilities
 # https://github.com/gerardnico/kube-x
 ##############################
-export KUBE_X_APP_HOME="$HOME/code/argocd:$HOME/code/infra/kustomize"
+export KUBE_X_APP_HOME="$HOME/code/argocd/apps:$HOME/code/argocd/infra"
 export KUBE_X_CLUSTER="beau"
 export KUBE_X_USER="default"
 export KUBE_X_KUBECTL="kubectx"
