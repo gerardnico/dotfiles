44c5a8201b75940c69f7391b7347b9db56a5839a
Nicolas GERARD <gerardnico@gmail.com>
2024-11-29 10:18:57 +0100

    Terminal mode

--- dot_bashenv.d/ans-x.sh
+++ dot_bashenv.d/ans-x.sh
@@ -7,11 +7,6 @@ export ANS_X_VAULT_ID_PASS=ansible/vault-password
 # SSH Key
 export ANS_X_SSH_KEY_PASS=ssh-x/id_gerardnico_kube
 
-# Project Dir
-ANS_X_PROJECT_DIR=$HOME/code/infra
-if [ -d "$ANS_X_PROJECT_DIR" ]; then
-  export ANS_X_PROJECT_DIR
-fi
 
 # Bin in Path (Add the ansible scripts)
 ANS_X_BIN_DIR=$HOME/code/ansible/bin
@@ -22,9 +17,14 @@ fi
 ##############################
 # Ansible Docker Env
 ##############################
+# Project Dir
+#ANS_X_PROJECT_DIR=$HOME/code/infra
+#if [ -d "$ANS_X_PROJECT_DIR" ]; then
+#  export ANS_X_PROJECT_DIR
+#fi
 # https://github.com/gerardnico/ansible
-export ANSIBLE_CONFIG=ansible.cfg
-export ANSIBLE_HOME=ansible
+#export ANSIBLE_CONFIG=ansible.cfg
+#export ANSIBLE_HOME=ansible
 
 
 
