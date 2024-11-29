0be221d0e69541f810f9a056df25af6f31701801
Nicolas GERARD <gerardnico@gmail.com>
2024-11-29 15:20:09 +0100

    New 9.12 ansible with Python pipx venv

--- dot_bashrc.d/python.sh
+++ dot_bashrc.d/python.sh
@@ -3,3 +3,14 @@
 if [ -x "$(command -v python3)" ]; then
   alias python=python3
 fi
+
+# Activate a venv
+# so that we don't get any
+# python externally managed
+# when executing a `pip3 install`
+# https://peps.python.org/pep-0668/
+PYTHON_VENV="$HOME/.venv"
+if [ ! -d "$PYTHON_VENV" ]; then
+  python3 -m venv "$PYTHON_VENV"
+fi
+source "$PYTHON_VENV/bin/activate"
