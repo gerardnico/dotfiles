45b0c53ebfaed9c2ab8596eb24078b63c69176e4
Nicolas GERARD <gerardnico@gmail.com>
2024-12-08 22:40:14 +0100

    Added tracert

--- dot_bashenv.d/kube.sh
+++ dot_bashenv.d/kube.sh
@@ -6,8 +6,6 @@
 export KUBE_X_APP_HOME="$HOME/code/argocd/apps:$HOME/code/argocd/infra"
 export KUBE_X_CLUSTER="beau"
 export KUBE_X_USER="default"
-export KUBE_X_KUBECTL="kubectx"
-export KUBE_X_BUSYBOX_IMAGE="ghcr.io/gerardnico/busybox@sha256:28e4627ee3c9f96eb36cb80de1acd7a8aa1e54b0940012ddb748ace5789954d6"
 
 # Alert Manager Connection for the API
 export KUBE_X_ALERT_MANAGER_URL=https://alertmanager.eraldy.com
