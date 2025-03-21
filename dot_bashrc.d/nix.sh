90ed1f0fac4a8a5dadfa40fb5f854e209f182610
Nicolas GERARD <gerardnico@gmail.com>
2025-03-21 10:40:18 +0100

    feat: add nix build

--- dot_bashrc.d/nix.sh
+++ dot_bashrc.d/nix.sh
@@ -6,5 +6,10 @@ if [ -e "$HOME"/.nix-profile/etc/profile.d/nix.sh ]; then
   . "$HOME"/.nix-profile/etc/profile.d/nix.sh;
 fi
 
-# https://nix.dev/manual/nix#sec-channels
-export NIX_PATH="nixpkgs=channel:nixos-22.25 nix-build"
+# Nix package path
+# The path are separated by space
+# for now, only a channel, but people generally add a `nix-build` directory
+# https://nix.dev/manual/nix#sec-channels / https://nixos.wiki/wiki/Nix_channels
+# Most users will want the stable/large channel
+# https://channels.nixos.org/
+export NIX_PATH="nixpkgs=channel:nixos-24.11"
