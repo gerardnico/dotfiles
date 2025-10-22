6504c1be390bd2cbeb0ce8b89621b5bbdb1d1538
Nicolas GERARD <gerardnico@gmail.com>
2025-10-22 14:06:51 +0200

    feat: added git env by company

--- dot_profile.d/combostrap.sh
+++ dot_profile.d/combostrap.sh
@@ -1,4 +1,5 @@
 
 # Git conf for combostrap project
 export COMBOSTRAP_GIT_EMAIL="nico@combostrap.com"
-
+# Long format
+export COMBOSTRAP_GIT_SIGNING_KEY="21B4F6A5CE5BA34E"
---
+++ dot_profile.d/tabulify.sh
@@ -0,0 +1,5 @@
+
+# Git conf for tabulify projects
+export TABULIFY_GIT_EMAIL="nico@tabulify"
+# Long format
+export TABULIFY_GIT_SIGNING_KEY="21B4F6A5CE5BA34E"
