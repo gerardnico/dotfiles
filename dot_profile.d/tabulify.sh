d2369354ac6d8a622c71d5c1f084d3b351e3d0a9
Nicolas GERARD <gerardnico@gmail.com>
2025-11-10 16:42:19 +0100

    fix: git conf for tabulify project

--- dot_profile.d/tabulify.sh
+++ dot_profile.d/tabulify.sh
@@ -1,5 +1,9 @@
 
+# Git Conf for Tabulify Project
+# Git as prefix because this is a env used for git
+# TABULIFY was conflicting with the env of the Tabulify cli
+
 # Git conf for tabulify projects
-export TABULIFY_GIT_EMAIL="nico@tabulify"
+export GIT_TABULIFY_EMAIL="nico@tabulify.com"
 # Long format
-export TABULIFY_GIT_SIGNING_KEY="21B4F6A5CE5BA34E"
+export GIT_TABULIFY_SIGNING_KEY="21B4F6A5CE5BA34E"
