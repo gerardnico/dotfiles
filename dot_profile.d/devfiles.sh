d97803977ba01f27e749dde402494eaa63997a40
Nicolas GERARD <nico@gerardnico.com>
2025-11-20 12:02:13 +0100

    refactor: repm to devf for devfiles

---
+++ dot_profile.d/devfiles.sh
@@ -0,0 +1,28 @@
+# Git conf for combostrap project
+# We start with git so that we see that it's a git config
+export DEVF_COMBOSTRAP_EMAIL="nico@combostrap.com"
+# Long format
+export DEVF_COMBOSTRAP_SIGNING_KEY="21B4F6A5CE5BA34E"
+
+
+###########
+# Tabulify
+###########
+# Git conf for tabulify projects
+export DEVF_TABULIFY_EMAIL="nico@tabulify.com"
+# Long format
+export DEVF_TABULIFY_SIGNING_KEY="$DEVF_COMBOSTRAP_SIGNING_KEY"
+
+# Repo Manager location
+export DEVF_TABULIFY_DIR=$HOME/code/combostrap/repo-manager
+
+###########
+# Gerardnico
+###########
+# Git conf for tabulify projects
+export DEVF_GERARDNICO_EMAIL="nico@gerardnico.com"
+# Long format
+export DEVF_GERARDNICO_SIGNING_KEY="$DEVF_COMBOSTRAP_SIGNING_KEY"
+
+# Repo Manager location
+export DEVF_GERARDNICO_DIR=$HOME/code/combostrap/repo-manager
