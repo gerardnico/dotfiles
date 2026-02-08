f74eba728e8591ba91207bd9401607117222961d
Nicolas GERARD <nico@gerardnico.com>
2026-02-08 18:56:36 +0100

    fix: bug on agent init rename

--- dot_bashrc.d/ssh-x.sh
+++ dot_bashrc.d/ssh-x.sh
@@ -3,10 +3,10 @@
 ##############################
 
 # Start the agent only in interactive mode
-# Otherwise we get a recursion because `ssh-x-agent-init`
+# Otherwise we get a recursion because `ssh-x-agent-start`
 # is also a script (ie non-interactive bash)
 # and will called this env
-if ! eval "$(source ssh-x-agent-init)"; then
+if ! eval "$(source ssh-x-agent-start)"; then
   echo "SSH Agent Started Not successfully" >/dev/tty
 fi
 
