83bc3202b2beb58f2d312daa0222c1eb3c704692
Nicolas GERARD <nico@gerardnico.com>
2026-02-11 11:07:54 +0100

    feat: added svgo

---
+++ dot_bashrc.d/svgo.sh
@@ -0,0 +1,3 @@
+
+# Unfortunately, svgo walks up the directory tree to find svgo.config.js
+alias svgo='svgo --config ~/.config/svgo/svgo.config.js'
---
+++ dot_config/svgo/svgo.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [
+    // https://svgo.dev/docs/preset-default/
+    'preset-default'
+  ]
+}
