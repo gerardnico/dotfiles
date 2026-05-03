6d992ff46377d10b44a7dac200a6a4cdc1613e63
Nicolas GERARD <nico@gerardnico.com>
2026-05-03 12:04:44 +0200

    feat: added crictl

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -2060,6 +2060,11 @@ main_brew() {
   # install github cli
   install_github_cli_brew_winget
 
+  # install crictl
+  # https://formulae.brew.sh/formula/cri-tools
+  # https://github.com/kubernetes-sigs/cri-tools/releases
+  util_install_brew "cri-tools"
+
   # install editor config checker
   install_editorconfig_checker_brew
 
