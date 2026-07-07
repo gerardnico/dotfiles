f7dc1a5189eac9ec66348e37842bfe307b7f61c1
Nicolas GERARD <nico@gerardnico.com>
2026-07-07 12:32:32 +0200

    fix: new cluster

--- dot_profile.d/kubee.sh
+++ dot_profile.d/kubee.sh
@@ -14,14 +14,14 @@ fi
 
 export KUBEE_CHARTS_PATH="$HOME/code/kube-argocd/charts"
 export KUBEE_CLUSTERS_PATH="$HOME/code/kube-argocd/clusters"
-export KUBEE_CLUSTER_NAME="beau"
+export KUBEE_CLUSTER_NAME="beau-hels"
 
 # kube env
 export HELM_BIN="kubee helm"
 export HELM_MAX_HISTORY=3
 
 # Alert Manager Connection for the API
-KUBEE_SERVER="188-245-43-250"
+KUBEE_SERVER="65-109-238-84"
 export KUBEE_ALERT_MANAGER_URL="https://alertmanager-${KUBEE_SERVER}.sslip.io/#/alerts"
 export KUBEE_ALERT_MANAGER_BASIC_AUTH_PASS_USER=alert-manager/user
 export KUBEE_ALERT_MANAGER_BASIC_AUTH_PASS_PASSWORD=alert-manager/password
